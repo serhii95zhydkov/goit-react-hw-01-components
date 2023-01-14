520ec06a8c69c3a239bf31249d86661e9d492a87
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-14 21:33:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
-import Profile from "./Profile/Profile";
-import user from '../data/user.json'
-
-// console.log(user);
+import Profile from './Profile/Profile';
+import user from '../data/user.json';
 
 export const App = () => {
   return (
