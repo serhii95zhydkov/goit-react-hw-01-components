02782c608509fe8e708f4c6825587b843341f1ee
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-14 23:11:53 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,1 +1,32 @@
 import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(data => {
+          return (
+            <li key={data.id} className="item">
+              <span className="label">{data.label}</span>
+              <span className="percentage">{data.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
+
+export default Statistics;
