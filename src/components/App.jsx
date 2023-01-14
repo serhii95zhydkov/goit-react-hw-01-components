bd3fc27e5ddcb33da2fd561af1ca1c79410d3c83
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-14 17:09:57 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import Profile from "./Profile/Profile";
+import user from '../data/user.json'
+
+// console.log(user);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile user={user} />
+    </>
   );
 };
