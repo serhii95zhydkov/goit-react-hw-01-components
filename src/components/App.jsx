85a9ce244b4ff823514e2d15f7423547afda46ba
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-14 23:11:43 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import user from '../data/user.json';
+import data from '../data/data.json';
+
 
 export const App = () => {
   return (
     <>
       <Profile user={user} />
+      <Statistics title="Upload stats" stats={data}/>
     </>
   );
 };
