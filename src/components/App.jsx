744cea15bf8232b43fcfca5791d12c8a32b353b0
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-15 21:30:19 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
 
 export const App = () => {
   return (
     <>
       <Profile user={user} />
-      <Statistics title="Upload stats" stats={data}/>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
