8c90a0be23e30650050f37cc574e255caad1dcf7
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-15 21:30:37 +0200

    Create FriendListItem.jsx

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+  return (
+    <>
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
+    </>
+  );
+};
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
+export default FriendListItem;
