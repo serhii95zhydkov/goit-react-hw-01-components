cf89e12a9fad7ef563607aa866c89c0a939edae1
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-15 21:30:26 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,1 +1,26 @@
 import PropTypes from 'prop-types';
+import FriendListItem from '../FriendListItem/FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => {
+        return (
+          <li key={friend.id} className="item">
+            <FriendListItem friend={friend} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
+
+export default FriendList;
