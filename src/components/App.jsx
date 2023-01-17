ea22e331e5c07d566200fe5ca5f2d8a6eb5f2373
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-01-17 20:00:15 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,18 @@ import transactions from '../data/transactions.json';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory'
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
     <>
-      <Profile user={user} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
